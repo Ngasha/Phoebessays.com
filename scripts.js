2b35c7f012f1396349cf760f227ffead9489638d
Peter Waweru <peter@Peter-Wawerus-MA.local>
2024-06-11 20:29:41 +0300

    menu button

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+  function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  }
+
