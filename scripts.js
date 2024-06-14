8f2b1f28400c7da6f0ec9b2e1ee30002e3edb4bf
Peter Waweru <peter@Peter-Wawerus-MA.local>
2024-06-14 21:48:29 +0300

    seo

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
-const hamburger = document.getElementById('hamburger');
-const mobileNav = document.getElementById('mobile-nav');
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const mobileNav = document.getElementById('mobile-nav');
 
-hamburger.addEventListener('click', () => {
-    mobileNav.classList.toggle('show');
-    hamburger.classList.toggle('open');
-});
+  hamburger.addEventListener('click', () => {
+      mobileNav.classList.toggle('show');
+      hamburger.classList.toggle('open');
+  });
 
-// Close mobile nav when clicking outside
-document.addEventListener('click', (e) => {
-    if (!hamburger.contains(e.target) && !mobileNav.contains(e.target)) {
-        mobileNav.classList.remove('show');
-        hamburger.classList.remove('open');
-    }
+  // Close mobile nav when clicking outside
+  document.addEventListener('click', (e) => {
+      if (!hamburger.contains(e.target) && !mobileNav.contains(e.target)) {
+          mobileNav.classList.remove('show');
+          hamburger.classList.remove('open');
+      }
+  });
 });
